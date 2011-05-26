bdc4fcf0351d770121bb94a5e5cbd747f183f7ef
David Watson <david@upwave.com>
2011-05-26 12:38:37 -0700

    Move to DSS signature verification

--- lib/roip_text_access_token.rb
+++ lib/roip_text_access_token.rb
@@ -1,6 +1,7 @@
 require 'addressable/uri'
 
 class RoipTextAccessToken
+  include RoipTokenAuth
   attr_reader :access_token, :scope, :valid_to, :signature, :namespace, :refresh
   
   def initialize(h)
@@ -16,7 +17,7 @@ class RoipTextAccessToken
     reqUriURI = Addressable::URI.parse(path)
     reqUriPQ = reqUriURI.path + (reqUriURI.query ? ("?" + reqUriURI.query) : "")
     if (!reqUriPQ.match(Regexp.escape(scopePQ)).nil? &&
-    (Time.zone.parse(self.valid_to).future?) && validate_signature && validate_namespaces)
+    (Time.zone.parse(self.valid_to).future?) && dss_validate_signature && validate_namespaces)
       Rails::logger.debug "Token is valid"
       return true
     else
@@ -26,6 +27,40 @@ class RoipTextAccessToken
   end
   
   private
+  
+  
+  def token_digest
+    OpenSSL::Digest::SHA1.digest(self.access_token +
+    self.scope +
+    self.valid_to +
+    (self.namespace.present? ? self.namespace : ""))
+  end
+    
+    
+  def dss_validate_signature
+    # TODO: support >1 CAS by iterating through keys array
+    pubkey = OpenSSL::PKey::DSA.new(OpenSSL::PKey::DSA.new(cas_public_dss_keys[0]))
+    if (pubkey.sysverify(token_digest, Base64.decode(self.signature)))
+      Rails::logger.debug "DSS Signature is valid"
+      return true
+    else
+      Rails.logger.debug "DSS Signature #{self.signature} is NOT valid"
+      return false
+    end
+  end
+  
+  
+  def validate_namespaces
+    return true if ! self.namespace
+    if !::RoipTokenAuth::ok_namespaces.empty? && !ok_namespaces.include?(self.namespace)
+      Rails::logger.warn "Found unacceptable namespace of #{self.namespace} in Access Token"
+      return false
+    end
+    true
+  end
+  
+  
+  # The following two methods are DEPRECATED pre-DSS versions
   
   def validate_signature
     [ self.access_token, self.scope, self.valid_to ].map { |i| return false if ! i }
@@ -41,17 +76,8 @@ class RoipTextAccessToken
     end
   end
   
+  
   def sign(str)
     "Provisional-" + ActiveSupport::Base64.encode64s(Digest::SHA2.new(512).digest(str + ::RoipTokenAuth::SIGNINGSECRET))
-  end
-  
-  
-  def validate_namespaces
-    return true if ! self.namespace
-    if !::RoipTokenAuth::ok_namespaces.empty? && !::RoipTokenAuth::ok_namespaces.include?(self.namespace)
-      Rails::logger.warn "Found unacceptable namespace of #{self.namespace} in Access Token"
-      return false
-    end
-    true
   end
 end
--- lib/roip_token_auth.rb
+++ lib/roip_token_auth.rb
@@ -1,18 +1,22 @@
 module RoipTokenAuth
+
+  mattr_accessor :ok_namespaces, :cas_public_dss_keys
   
-  mattr_accessor :ok_namespaces
-  @@ok_namespaces = ["okn1"]
-  
+  # Configure these in the parent app's config/initializers/roip_token_auth.rb, e.g.:
+  #    RoipTokenAuth::ok_namespaces = [ "foo", "bar", "baz/foobar" ]
+  @@ok_namespaces = [] # The list of namespaces this server will accept and serve - should include its primary hostname
+  @@cas_public_dss_keys = [] # The list of DSS public keys in PEM format, one per Authorization Server whose access tokens are trusted
+
   require 'engine' if defined?(Rails)
 
   # VITAL for security: replace this technique with a CA-based Signature cert
   SIGNINGSECRET = "mugwhump"
-  
+
   module Controllers
     module Helpers
       def roip_token_filter
 
-	logger.debug "Header: #{request.headers["HTTP_AUTHORIZATION"]}"
+        logger.debug "Header: #{request.headers["HTTP_AUTHORIZATION"]}"
         if request.headers["HTTP_AUTHORIZATION"] && 
           ((oauth_string = request.headers["HTTP_AUTHORIZATION"].gsub!(/^.*Oauth /, "")).size > 0)
           theToken = RoipTextAccessToken.new(JSON.parse oauth_string)
