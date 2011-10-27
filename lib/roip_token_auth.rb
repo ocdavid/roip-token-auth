5d1b077986b95013279d393d140c770e73f13ab2
David Watson <david@upwave.com>
2011-10-27 16:49:48 -0700

    Auth header test

--- lib/roip_token_auth.rb
+++ lib/roip_token_auth.rb
@@ -50,7 +50,7 @@ module RoipTokenAuth
         if request.method == :options
           headers['Access-Control-Allow-Origin'] = '*'
           headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-          headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+          headers['Access-Control-Allow-Headers'] = 'Authorization, X-Requested-With, X-Prototype-Version'
           headers['Access-Control-Max-Age'] = '1728000'
           render :text => '', :content_type => 'text/plain'
         end
--- roip_token_auth.gemspec
+++ roip_token_auth.gemspec
@@ -5,7 +5,7 @@
 
 Gem::Specification.new do |s|
   s.name = "roip_token_auth"
-  s.version = "0.3.0"
+  s.version = "0.3.1"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["David Watson"]
