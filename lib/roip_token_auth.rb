8ea7aacad954d2d153cdc571f78561c6a347fbe0
David Watson <david@upwave.com>
2011-10-27 15:24:14 -0700

    Comments; allow AJAX reqs from anywhere to fail if appropriate.

--- lib/roip_token_auth.rb
+++ lib/roip_token_auth.rb
@@ -2,6 +2,13 @@ module RoipTokenAuth
 
   mattr_accessor :cas_public_dss_keys
   
+  # Important note: To allow AJAX requests from anywhere to pick up token-validated assets,
+  # set a header line as follows in the controller that initiates the response:
+  #  response.headers["Access-Control-Allow-Origin"] = "*" # For XMLHttpRequests from anywhere
+  # This is safe since only token-authorized requests will be presented as long as
+  #   before_filter :roip_token_filter
+  # is used to filter hits to the asset endpoint.
+
   # Configure these in the parent app's config/initializers/roip_token_auth.rb, e.g.:
   #    RoipTokenAuth::cas_public_dss_keys = [ "-----BEGIN PUBLIC KEY-----..." ]
   @@cas_public_dss_keys = [] # The list of DSS public keys in PEM format, one per Authorization Server whose access tokens are trusted
@@ -19,6 +26,7 @@ module RoipTokenAuth
 
         if ! (theToken && theToken.valid?(request.fullpath, request.request_method))
           # TODO: Allow the parent app to set a "failure" route
+          response.headers["Access-Control-Allow-Origin"] = "*" # For XMLHttpRequests from anywhere
           render :text => "Invalid RoIP Token", :status => 401
         end
       end
